2a9250a8506832228d34de0e654f7dc439913a4e
Rob <rob_abcba@yahoo.com>
2018-11-29 10:09:11 -0800

    MainActivity.java: Add Log.e in 3 locations in the code to log errors (2 catch IOExceptions + 1 note ResponseCode not 200 = success).

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -114,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 jsonResponse = makeHttpRequest(url);
             } catch (IOException e) {
                 // TODO Handle the IOException
+                Log.e(LOG_TAG, "Problem making the HTTP request.", e);
             }
 
             // Extract relevant fields from the JSON response and create an {@link Event} object
@@ -180,9 +181,12 @@ public class MainActivity extends AppCompatActivity {
                 if (urlConnection.getResponseCode() == 200) {
                 inputStream = urlConnection.getInputStream();
                 jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
                 }
             } catch (IOException e) {
                 // TODO: Handle the exception
+                Log.e(LOG_TAG, "Oh fudge.  We got an IOException e = ", e);
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
